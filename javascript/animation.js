557a0c7b4bd19288507dbb21a94020b944624e5d
Hamaad Chughtai <Hamaad.Chughtai102@gmail.com>
2016-06-30 15:16:19 -0400

    Added the animations. As it stands now the site is just missing my picture and the icons beside it. Portfolio section will be undergo redesign, however.

---
+++ javascript/animation.js
@@ -0,0 +1,28 @@
+$(window).scroll(function(){
+   
+    console.log($(this).scrollTop);
+    
+    if($(this).scrollTop()>=1000){
+        $('.one').addClass('animate');
+        $('.two').addClass('animate');
+        $('.three').addClass('animate');
+        $('.four').addClass('animate');
+        $('.five').addClass('animate');
+    }
+    
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
